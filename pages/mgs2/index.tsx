8e09e40e741713c7c12ee58dbd2a299d74b60947
Jérôme Grossé <jerome.l.grosse@gmail.com>
2021-06-12 19:22:41 +0200

    Create a basic mgs2 index page

--- pages/mgs2/index.tsx
+++ pages/mgs2/index.tsx
@@ -1,5 +1,30 @@
-const Mgs2PagesList = () => {
-    return <p>This is the page list</p>;
+import fs from 'fs';
+import { GetStaticProps } from 'next';
+import path from 'path';
+import Link from 'next/link'
+
+
+const Mgs2PagesList = ({posts}) => {
+    return (
+        <ul>
+            {posts.map(p => (
+                <li key={p}><Link href={`/mgs2/${encodeURIComponent(p)}`}>{p}</Link></li>
+            ))}
+        </ul>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const jsonFilesLocation = path.join(process.cwd(), '/public/mgs2/');
+    const directoryContent = fs.readdirSync(jsonFilesLocation);
+    const jsonFiles = directoryContent.filter((filename) => filename.match(/\.(json)$/)) // Filter all file so we only get the JSON ones
+        .map(filename => filename.replace('.json', '')); // Remove .json extension
+
+    
+
+    return {
+        props: {posts: jsonFiles}
+    }
 }
 
 
